a5f2f33de56a1301c35256c538fe58d4d76ff52d
unknown <dlwlzjffj1@naver.com>
2023-01-17 13:52:26 +0900

    2023/01/17 가상브라우저 실습

--- Crawling/2.HTML 파싱.py
+++ Crawling/2.HTML 파싱.py
@@ -1,13 +1,13 @@
 """
 날짜 : 2023/01/16
-이름 : 공민혁
-내용 : 파이썬 HTML 파싱 실습
+이름 : 김철학
+내용 : 파이썬 HTML 파싱 실습하기
 """
 import requests as req
 from bs4 import BeautifulSoup as bs
 
 # HTML 요청
-url = 'http://chhak.click/parsing/sample1.html'
+url = 'http://chhak.click/parsing/sample2.html'
 html = req.get(url).text
 
 print(html)
@@ -17,6 +17,11 @@ dom = bs(html, 'html.parser')
 
 # 문서파싱
 tit = dom.html.body.h1.text
-desc = dom.select_one('#txt').text
+txt = dom.select_one('#txt').text
+lis = dom.select('ul > li')
+
 print('tit :', tit)
 print('txt :', txt)
+
+for li in lis:
+    print('li text :', li.text)
